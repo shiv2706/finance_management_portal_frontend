261d71bbbf47c4c6a82e946a611663ea925bea59
Shivansh Pradhan <shivansh4858@gmail.com>
2025-02-09 01:31:27 +0530

    added responsive features

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    {/*<div className="collapse navbar-collapse" id="navbarTogglerDemo01">*/}
                         <Link className="navbar-brand" to="/">
                             Finance Portal <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
                                  className="bi bi-cash-coin" viewBox="0 0 16 16">
@@ -62,24 +62,9 @@ const Header = () => {
                                     </svg>  {loginUser && loginUser.name}</button>
                             </li>
 
-                            {/*<li className="nav-item">*/}
-                            {/*    <button className="btn btn-primary"*/}
-                            {/*            onClick={logoutHandler}>*/}
-                            {/*        <h8>Logout  </h8>*/}
-                            {/*        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"*/}
-                            {/*             className="bi bi-box-arrow-right" viewBox="0 0 16 16">*/}
-                            {/*            <path fill-rule="evenodd"*/}
-                            {/*                  d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1*/}
-                            {/*                   .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5*/}
-                            {/*                    1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>*/}
-                            {/*            <path fill-rule="evenodd"*/}
-                            {/*                  d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5*/}
-                            {/*                  0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>*/}
-                            {/*        </svg>*/}
-                            {/*    </button>*/}
-                            {/*</li>*/}
+
                         </ul>
-                    </div>
+                    {/*</div>*/}
                 </div>
                 <Modal className="allModal" title="User Info" open={showUser} onCancel={() => {
                     setShowUser(false)
