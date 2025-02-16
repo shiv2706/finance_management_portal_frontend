1c89fa96699b5f0a0380a387c636bfd2b6682455
Shivansh Pradhan <shivansh4858@gmail.com>
2025-02-17 01:01:46 +0530

    image upload status checkpoints added

---
+++ src/components/CategorizingData.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CategorizingData = () => {
+    return (
+        <>
+            <div className="msg justify-content-center">
+                <div className="success" role="status">
+                    <h8>Categorizing your Transaction...</h8>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CategorizingData;
---
+++ src/components/ExtractingText.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ExtractingText = () => {
+    return (
+        <>
+            <div className="msg justify-content-center">
+                <div className="success" role="status">
+                    <h8>Extracting Transaction Details...</h8>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ExtractingText;
