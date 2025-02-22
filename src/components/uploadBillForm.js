a42b1c5dbc23c79336f99aad803ff25790f1381f
Shivansh Pradhan <shivansh4858@gmail.com>
2025-02-22 18:48:07 +0530

    added camera image click option

--- src/components/uploadBillForm.js
+++ src/components/uploadBillForm.js
@@ -31,7 +31,7 @@ const UploadBillForm = () => {
             input.onchange = (event) => {
                 const file = event.target.files[0];
                 if (file) {
-                    setFileList([{ uid: Date.now(), name: file.name, originFileObj: file }]);
+                    setFileList([{ uid:`${Date.now()}`, name: file.name, originFileObj: file }]);
                 }
             };
 
@@ -130,15 +130,7 @@ const UploadBillForm = () => {
             {success && <Success/>}
             {showprogressBar && <UploadProgress value={progress}/>}
             {error && <Failed />}
-            <Button onClick={captureImage} style={{marginBottom:10}}>
-                Capture Image <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" fill="currentColor"
-                                   className="bi bi-camera" viewBox="0 0 16 16">
-                <path
-                    d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
-                <path
-                    d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
-            </svg>
-            </Button>
+
             <Form.Item
                 name="file"
                 rules={[{required: true, message: "Please upload a file."}]}
@@ -160,6 +152,15 @@ const UploadBillForm = () => {
                     </Button>
                 </Upload>
             </Form.Item>
+            <Button onClick={captureImage} style={{marginTop:0}}>
+                Capture Image <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" fill="currentColor"
+                                   className="bi bi-camera" viewBox="0 0 16 16">
+                <path
+                    d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z"/>
+                <path
+                    d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0"/>
+            </svg>
+            </Button>
 
 
             <div className="d-flex justify-content-end">
